8c28933dd1d4a3d86efab6e0927c44b0fa941324
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-05-09 17:33:42 +0200

    Regeneration using Flexion

--- Flexions/WattPlayer/variables.php
+++ Flexions/WattPlayer/variables.php
@@ -9,11 +9,11 @@ if (isset ( $f )) {
 	$f->company = "Benoit Pereira da Silva";
 	$f->prefix = $prefix;
 	$f->author = "benoit@pereira-da-silva.com";
-	$f->projectName = "Watt-Multimedia-Engine";
+	$f->projectName = "Watt";
 	$f->license = FLEXIONS_ROOT_DIR."flexions/helpers/licenses/LGPL.tpl.php";
 }
 
-$imports = "\n#import <Foundation/Foundation.h>\n\n";
-$parentClass = "NSObject";
+$imports = "\n#import \"WTMObject.h\"\n\n";
+$parentClass = "WTMObject";
 $markAsDynamic = false;
 $allowScalars = true;
