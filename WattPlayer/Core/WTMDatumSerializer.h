468664bad9ce5ede4f252d7e78fb5995875ea6da
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-05-09 17:28:11 +0200

    Datum serializer

---
+++ WattPlayer/Core/WTMDatumSerializer.h
@@ -0,0 +1,42 @@
+// This file is part of "Watt"
+//
+// "Watt" is free software: you can redistribute it and/or modify
+// it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// "Watt" is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU LESSER GENERAL PUBLIC LICENSE for more details.
+//
+// You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+// along with "Watt"  If not, see <http://www.gnu.org/licenses/>
+//
+//  WTMDatumSerializer.h
+//  PlayerSample
+//
+//  Created by Benoit Pereira da Silva on 09/05/13.
+//  Copyright (c) 2013 Pereira da Silva. All rights reserved.
+//
+
+
+/*
+ 
+    This serializer encodes any WTMObject  to be serialized in a package.
+    
+    ProjectSoundMember <- WTMember.h
+    ProjectSoundMember has a property : NSString * artist
+ 
+    To serialize the ProjectSoundMember to a Watt package 
+    We encode its "extra" properties within a WTMDatum
+ 
+    We store all those data in a WTMCollectionOfDatum
+ 
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface WTMDatumSerializer : NSObject
+
+@end
