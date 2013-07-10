5f50f914e7e2c7568f5f4e87571e880e8e5209af
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-07-10 10:43:15 +0200

    Creation of WattAcl

---
+++ WattPlayer/Core/WattAcl.m
@@ -0,0 +1,23 @@
+//
+//  WattAcl.m
+//  PlayerSample
+//
+//  Created by Benoit Pereira da Silva on 10/07/13.
+//  Copyright (c) 2013 Pereira da Silva. All rights reserved.
+//
+
+#import "WattAcl.h"
+
+@implementation WattAcl
+
+
+-(NSString*)stringRightsFrom:(NSUInteger)numericRights{
+
+}
+
+-(NSUInteger*)numericRightsFromString:(NSString*)stringRights{
+    
+}
+
+
+@end
