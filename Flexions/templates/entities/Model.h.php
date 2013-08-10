19d3d0eed700dc084347f3f3f5eedb8cdfd0ade9
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-08-10 14:45:22 +0200

    Cleaning

--- Flexions/templates/entities/Model.h.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include FLEXIONS_SOURCE_DIR.'/variables.php';
-include FLEXIONS_ROOT_DIR.'flexions/helpers/languages/Objective-c/ObjCModel.h.php';
-
-?>
--- Flexions/templates/entities/Model.m.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include FLEXIONS_SOURCE_DIR.'/variables.php';
-include FLEXIONS_ROOT_DIR.'flexions/helpers/languages/Objective-c/ObjCModel.m.php';
-?>
