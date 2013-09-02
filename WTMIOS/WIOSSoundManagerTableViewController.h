b52ef692dd45074e428753495aa07eb25f24c451
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-09-02 17:53:18 +0200

    Creation of WTMIOS

---
+++ WTMIOS/WIOSSoundManagerTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  WIOSSoundManagerTableViewController.h
+//  PlayerSample
+//
+//  Created by Benoit Pereira da Silva on 02/09/13.
+//  Copyright (c) 2013 Pereira da Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WIOSSoundManagerTableViewController : UITableViewController
+
+@property (nonatomic,strong)    WTMLibrary *library;
+@property (nonatomic,readonly)  WTMCollectionOfMember *sounds;
+
+@end
