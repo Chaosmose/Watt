81fa6835c1952e61393f3aa3dae3b1f11501c98f
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2013-09-26 15:29:18 +0200

    Alert macro for IOS

--- WTMIOS/WIOS.h
+++ WTMIOS/WIOS.h
@@ -3,8 +3,13 @@
 //  Watt
 //
 //  Created by Benoit Pereira da Silva on 02/09/13.
-//  Copyright (c) 2013 HopToys. All rights reserved.
+//  Copyright (c) 2013 Benoit Pereira da Silva . All rights reserved.
 //
+
+
+#import <UIKit/UIKit.h>
+
+#define wtodo() [[[UIAlertView alloc] initWithTitle:@"!" message:@"Todo" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show]
 
 #ifndef WIOS_h
 #define WIOS_h
@@ -17,5 +22,6 @@
 #import "WIOSSoundListCell.h"
 #import "WIOSSoundRecorderViewController.h"
 #import "WIOSSoundManagerTableViewController.h"
+
 
 #endif
